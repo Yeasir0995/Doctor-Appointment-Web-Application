39318b349ed74828022555bd5954f0270f3b5368
Yeasir0995 <40072145+Yeasir0995@users.noreply.github.com>
2021-05-05 14:13:51 +0600

    Add files via upload

---
+++ resources/views/layouts/Untitled-1.php
@@ -0,0 +1,78 @@
+       <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+              <div class="card card-stats">
+                <div class="card-header card-header-warning card-header-icon">
+                  <div class="card-icon">
+                    <i class="material-icons">content_copy</i>
+                  </div>
+                  <p class="card-category">Free slot</p>
+                  <h3 class="card-title">10/20
+                    <small></small>
+                  </h3>
+                </div>
+                <div class="card-footer">
+                  <div class="stats">
+                    <i class="material-icons text-danger">warning</i>
+                    <a href="javascript:;">Get More Space...</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+              <div class="card card-stats">
+                <div class="card-header card-header-success card-header-icon">
+                  <div class="card-icon">
+                    <i class="material-icons">Doctor</i>
+                  </div>
+                  <p class="card-category">Number of Doctor</p>
+                  <h3 class="card-title">10</h3>
+                </div>
+                <div class="card-footer">
+                  <div class="stats">
+                    <i class="material-icons">date_range</i> Last 24 Hours
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+              <div class="card card-stats">
+                <div class="card-header card-header-danger card-header-icon">
+                  <div class="card-icon">
+                    <i class="material-icons">info_outline</i>
+                  </div>
+                  <p class="card-category">Fixed Issues</p>
+                  <h3 class="card-title">75</h3>
+                </div>
+                <div class="card-footer">
+                  <div class="stats">
+                    <i class="material-icons">local_offer</i> Tracked from Github
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+              <div class="card card-stats">
+                <div class="card-header card-header-info card-header-icon">
+                  <div class="card-icon">
+                    <i class="fa fa-twitter"></i>
+                  </div>
+                  <p class="card-category">Followers</p>
+                  <h3 class="card-title">+245</h3>
+                </div>
+                <div class="card-footer">
+                  <div class="stats">
+                    <i class="material-icons">update</i> Just Updated
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+          <div class="row">
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+          </div>
+        </div>
---
+++ resources/views/layouts/adminMaster.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Admin Dashboard
+  </title>
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="../assets/demo/demo.css" rel="stylesheet" />
+
+</head>
+
+<body class="">
+  <div class="wrapper " id="app">
+    <div class="sidebar" data-color="green" data-background-color="white" data-image="../assets/img/sidebar-1.jpg">
+      <div class="logo"><a href="" class="simple-text logo-normal">
+          Creative Tim
+        </a></div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+          <li class="nav-item active  ">
+            <router-link class="nav-link" to="/admin_dashboard">
+              <i class="material-icons">dashboard </i>
+              <p>Dashboard</p>
+            </router-link>
+          </li> 
+      @include('includes.adminSide')
+    <div class="main-panel">
+      <!-- Navbar -->
+         @include('includes.adminNavbar')
+      <!-- End Navbar -->
+      <div class="content" >
+        <div class="container-fluid">  
+        <h4 class="text-primary">Welcome {{ Auth::user()->firstname }}  </h4>    
+     <transition name="fade" mode="out-in" >
+      <router-view></router-view>
+      </transition>
+   
+      </div>
+      
+    </div>
+  </div>
+    </div>
+    <style>
+    .fade-enter,  .fade-leave-active {
+      opacity: 0;
+      
+    }
+    .fade-enter-active, .fade-leave-active {
+      transiton-duration: 0.3s;
+      transiton-property: opacity;
+      transition-timing-function: ease;
+    }
+   
+  
+    
+    </style>
+  <footer class="footer">
+        <div class="container-fluid">
+          <div class="copyright float-right">
+            &copy;
+            <script>
+              document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="" target="_blank">Yeasir</a> for a better web.
+          </div>
+        </div>
+      </footer>
+          <script src=" {{ asset('js/app.js') }}"></script>
+</body>
+         @include('includes.footerScript')
+
+</html>
